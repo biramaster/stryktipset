4b804f8b4ac9abb2df9cb5febb203117c9766198
biramaster <biramaster001@gmail.com>
2015-01-29 13:13:20 +0100

    Data presentation

--- StryktipsetVS/js/app.js
+++ StryktipsetVS/js/app.js
@@ -2,6 +2,20 @@
     var stryket = $.getJSON(
         "https://api.www.svenskaspel.se/draw/stryktipset/draws/result?callback=?");
     stryket.done(function (result) {
+        result["result"]["events"].forEach(function (game,i) {
+            console.log(game["participants"][0]["name"]+ " -VS- " +
+                        game["participants"][1]["name"]+ " Result: " + 
+                        game["outcome"]);
+            var tr = $("<tr/>");
+            tr.append("<td>" + (i+1) + "</td>");
+            tr.append("<td>" + game["participants"][0]["name"] + " -VS- " +
+                               game["participants"][1]["name"] + 
+                      "</td>");
+            tr.append("<td>" + game["outcome"] + "</td>");
+            tr.append("<td>" + game["outcome"] + "</td>");
+            tr.append("<td>" + game["outcome"] + "</td>");
+            $("#table").append(tr);
+        });
         console.log(result["result"]);
     });
 })();
