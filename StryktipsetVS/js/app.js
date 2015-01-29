ca50195d8497773ed876d8a8e63f744e1afb8d69
biramaster <biramaster001@gmail.com>
2015-01-29 12:24:19 +0100

    Get data via jSON

--- StryktipsetVS/js/app.js
+++ StryktipsetVS/js/app.js
@@ -1,3 +1,7 @@
 (function (app) {
-
-});
+    var stryket = $.getJSON(
+        "https://api.www.svenskaspel.se/draw/stryktipset/draws/result?callback=?");
+    stryket.done(function (result) {
+        console.log(result["result"]);
+    });
+})();
